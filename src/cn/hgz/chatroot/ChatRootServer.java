0ac40ed5fbfbc5668f315a6016b9735746b4a0a7
hgz <269072504@qq.com>
2019-08-02 18:17:51 +0800

    增加监听

--- src/cn/hgz/chatroot/ChatRootServer.java
+++ src/cn/hgz/chatroot/ChatRootServer.java
@@ -1,8 +1,39 @@
 package cn.hgz.chatroot;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ChatRootServer {
+	public static final int PORT= 9080;
 	public static void main(String[] args) {
-		
+		new ChatRootServer().init();
+	
+	}
+	
+	
+	public void init() {
+		try {
+			ServerSocket serversocket= new ServerSocket(PORT);
+			while(true) {
+				Socket sa= serversocket.accept();
+				System.out.println("有计算机连接到服务器");
+				
+				new Thread (new Runnable() {
+					
+					public void run() {
+						
+					}
+					
+				} ).start();
+			}
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("服务器异常"+e.getMessage());
+		}
 	}
 
 }
