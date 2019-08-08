524925924b070dcbdb43c460913019fef31b8ea3
hgz <269072504@qq.com>
2019-08-08 18:31:27 +0800

    测试添加按钮

--- src/cn/hgz/chatroot/ChatRootServer.java
+++ src/cn/hgz/chatroot/ChatRootServer.java
@@ -1,39 +1,48 @@
 package cn.hgz.chatroot;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class ChatRootServer {
 	public static final int PORT= 9080;
 	public static void main(String[] args) {
-		new ChatRootServer().init();
+		ChatRootServer  crs= new ChatRootServer();
+		//crs.init();
+		crs.JFrame();
+	
+		
+		
 	
 	}
 	
+	public void JFrame() {
+		JFrame frame = new JFrame();
+		frame.setLocation(200, 200);
+		frame.setSize(600,600);
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(3);
+		
+		
+		
+		JButton button = new JButton("发送111111");
+		button.setBounds(520, 520, 50, 60);
+	};
 	
-	public void init() {
-		try {
-			ServerSocket serversocket= new ServerSocket(PORT);
-			while(true) {
-				Socket sa= serversocket.accept();
-				System.out.println("有计算机连接到服务器");
-				
-				new Thread (new Runnable() {
-					
-					public void run() {
-						
-					}
-					
-				} ).start();
-			}
-			
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("服务器异常"+e.getMessage());
+	JPanel p = new JPanel() {
+		public void paintComponent() {
+			Button button1 = new Button("我是按钮");
+			button1.setBounds(520, 520, 50, 50);
 		}
-	}
+	};
+	
 
 }
